d3b45fca548f1086ee15265b3c1acb662f6a0e9f
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-16 11:38:31 -0500

    Used the arrow function to simplify the code.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(record => record.result === "W");
+
+    return winningRecord ? winningRecord.year : undefined;
+}
